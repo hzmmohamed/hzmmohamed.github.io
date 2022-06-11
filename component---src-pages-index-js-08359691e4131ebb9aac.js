55ee75679a865212b6019cf2f77173ce212b4fe4
Hazem Fahmi <hzmmohamed@gmail.com>
2022-06-11 05:49:32 +0200

    Updates

--- component---src-pages-index-js-08359691e4131ebb9aac.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkhzmmohamed=self.webpackChunkhzmmohamed||[]).push([[678],{7704:function(u,e,n){n.r(e);n(7294);var a=n(5444);e.default=function(){return(0,a.c4)("/about"),null}}}]);
-//# sourceMappingURL=component---src-pages-index-js-08359691e4131ebb9aac.js.map
